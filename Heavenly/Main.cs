7077912cdc762a7220439a60af0700e768e30a00
Pipsquexk <LHGames21@gmail.com>
2021-09-14 22:21:47 -0500

    Add project files.

---
+++ Heavenly/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using MelonLoader;
+
+namespace Heavenly
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            Console.Clear();
+            MelonUtils.SetConsoleTitle("Heavenly");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("                   ,                                 _        ");
+            Console.WriteLine("                  /|   |                            | |       ");
+            Console.WriteLine("                   |___|  _   __,        _   _  _   | |       ");
+            Console.WriteLine("                   |   |\\|/  /  |  |  |_|/  / |/ |  |/  |   | ");
+            Console.WriteLine("                   |   |/|__/\\_/|_/ \\/  |__/  |  |_/|__/ \\_/|/");
+            Console.WriteLine("                                                           /| ");
+            Console.WriteLine("                                                           \\| ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("                                    I\'m back");
+            Console.WriteLine(" ");
+            Console.WriteLine("                        ================================");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ||                            ||");
+            Console.WriteLine("                        ================================");
+            Patches.ApplyPatches();
+            MelonLogger.Msg("Fuck you");
+        }
+    }
+}
---
+++ Heavenly/Patches.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using HarmonyLib;
+
+namespace Heavenly
+{
+    public static class Patches
+    {
+
+        public static HarmonyLib.Harmony Instance;
+
+
+        public static HarmonyMethod GetLocalPatch(string name) { return new HarmonyMethod(typeof(Patches).GetMethod(name, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)); }
+
+        public static void ApplyPatches()
+        {
+            Instance = new HarmonyLib.Harmony("HeavenlyPatches");
+            //Instance.Patch(AccessTools.Property(typeof(MelonLogger), "DefaultMelonColor").GetMethod, GetLocalPatch("MelonColorPatch"));
+        }
+    }
+}
