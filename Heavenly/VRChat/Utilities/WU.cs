aa244d4ec654d1aea783cd19e711efbccf831d8d
Pipsquexk <LHGames21@gmail.com>
2022-07-29 12:52:41 -0500

    Update WU.cs

--- Heavenly/VRChat/Utilities/WU.cs
+++ Heavenly/VRChat/Utilities/WU.cs
@@ -8,11 +8,6 @@ namespace Heavenly.VRChat.Utilities
 {
     public static class WU
     {
-
-        public static string BuildInstanceID()
-        {
-            return RoomManager.field_Internal_Static_ApiWorldInstance_0.id;
-        }
-
+        public static string BuildInstanceID() => RoomManager.field_Internal_Static_ApiWorldInstance_0.id;
     }
 }
